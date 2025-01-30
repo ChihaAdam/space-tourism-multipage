243106ebed234c4b6b96e3698546640043a94972
ChihaAdam <chihaadam137@gmail.com>
2025-01-30 12:14:53 +0100

    simple changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,5 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import Link from "next/link";
 
 export const metadata = {
   title: "Create Next App",
@@ -19,20 +9,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <title>frontend-mentor : space tourism multipage</title>
+        <link rel="icon" href="../assets/favicon-32x32.png"></link>
+      </head>
       <body className="body">
-        <link rel="icon" hrsef="../assets/favicon-32x32.png"></link>
         <header>
+        <div className="lineContainer">
         <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48"><g fill="none" fillRule="evenodd"><circle cx="24" cy="24" r="24" fill="#FFF"/><path fill="#0B0D17" d="M24 0c0 16-8 24-24 24 15.718.114 23.718 8.114 24 24 0-16 8-24 24-24-16 0-24-8-24-24z"/></g></svg>
         <div className="line"></div>
+        </div>
         <nav>
-          <ul>
-            <li>
-              01 Home
-            </li>
-            <li></li>
-            <li></li>
-            <li></li>
-          </ul>
+              <Link className="Link" href="/"><p className="index">01</p> <p>HOME</p></Link>
+              <Link className="Link" href="/destination"><p className="index">02</p> <p>DESTINATION</p></Link>
+              <Link className="Link" href="#"><p className="index">03</p> <p>CREW</p></Link>
+              <Link className="Link" href="#"><p className="index">04</p> <p>TECHNOLOGIE</p></Link>
         </nav>
         </header>
         {children}
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,21 @@
 import styles from "./page.module.css";
-
 export default function Home() {
   return (
-    <div className={styles.page}>
-    </div>
+    <main className={styles.page}>
+      <section className={styles.article}>
+        <section className={styles.end}>
+        <div className={styles.left}>
+          <h4 className={styles.intro}>SO, YOU WANT TO TRAVEL TO</h4>
+          <h1 className={styles.space}>SPACE</h1>
+          <p className={styles.paragraph}>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
+        </div>
+        <div className={styles.right}>
+          <div className={styles.circle}>
+            <h3>explore</h3>
+          </div>
+        </div>
+        </section>
+      </section>
+    </main>
   );
 }
